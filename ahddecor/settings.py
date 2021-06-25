c74fa7979ce56aceb1aedfdf37af22c9aea0a779
Adarsh Sarda <adarsh.sarda@yahoo.com>
2021-06-25 21:05:37 +0530

    Price Filter

--- ahddecor/settings.py
+++ ahddecor/settings.py
@@ -25,6 +25,10 @@ INSTALLED_APPS = [
     'crispy_forms',
     'django_resized',
     'django_filters',
+<<<<<<< Updated upstream
+=======
+    #'bootstrapform',
+>>>>>>> Stashed changes
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
